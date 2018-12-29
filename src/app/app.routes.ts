52ef2970a2c902f4a84342fa7962536712cf2a90
Irasema <irasema@Marcos.local>
2018-12-29 17:11:50 -0500

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+// import { RouterModule, Routes } from '@angular/router';
+// import { Component } from '@angular/core';
+
+// const app-routes: Routes = [
+//     { path: 'home', component: Component},
+//     { path: '**', pathMach: 'full', redirecTo: 'home'}
+// ];
+
+// export const app_routing = RouterModule.forRoot(app-routes);
